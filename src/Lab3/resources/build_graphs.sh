b75bfa79565a1d04097dee25792c6f71b46cfd3a
MaximHordijchuk <hordijchuk.m.i@gmail.com>
2014-12-04 20:48:50 +0200

    Interpolation input points changed, changed script for building graphics

--- src/Lab3/resources/build_graphs.sh
+++ src/Lab3/resources/build_graphs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd res;
 for f in *.int; do
-echo "set term png \nset output \"${f%.*}.png\" \nplot \"$f\" smooth csplines, \"points.pts\" with points" | gnuplot;
+echo "set term png \nset grid \nset key below \nset title \"${f%.*}\" \nset output \"${f%.*}.png\" \nplot \"$f\" smooth unique lc rgb 'blue' title '${f%.*}', \"points.pts\" with points lc rgb 'red' title 'points'" | gnuplot;
 done;
